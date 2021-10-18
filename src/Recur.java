ee9d7b8d201ff091dab1661b21d22545a6031897
Zeiion <Zeiion@qq.com>
2021-10-18 20:55:12 +0800

    fix bugs

--- src/Recur.java
+++ src/Recur.java
@@ -4,53 +4,57 @@ public class Recur {
 	private static int index = 0;
 
 	public static String executeV(String s, List<String> list, int size) {
-		switch (s) {
-			case V.CompUnit:
-				return isCompUnit(list);
-			case V.FuncDef:
-				return isFuncDef(list);
-			case V.FuncType:
-				return isFuncType(list);
-			case V.INT:
-				return isVT(list, V.INT);
-			case V.Ident:
-				return isIdent(list);
-			case V.MAIN:
-				return isVT(list, V.MAIN);
-			case V.SBL:
-				return isVT(list, V.SBL);
-			case V.SBR:
-				return isVT(list, V.SBR);
-			case V.Block:
-				return isBlock(list);
-			case V.LBL:
-				return isVT(list, V.LBL);
-			case V.LBR:
-				return isVT(list, V.LBR);
-			case V.Stmt:
-				return isStmt(list);
-			case V.RETURN:
-				return isVT(list, V.RETURN);
-			case V.Number:
-				return isNumber(list);
-			case V.SEMICOLON:
-				return isVT(list, V.SEMICOLON);
-			case V.DecConst:
-				return isDecConst(list);
-			case V.OctConst:
-				return isOctConst(list);
-			case V.HexConst:
-				return isHexConst(list);
-			case V.NoneZeroDigit:
-				return isNoneZeroDigit(list);
-			case V.Digit:
-				return isDigit(list);
-			case V.OctDigit:
-				return isOctDigit(list);
-			case V.HexPre:
-				return isHexPre(list);
-			case V.HexDigit:
-				return isHexDigit(list);
+		try{
+			switch (s) {
+				case V.CompUnit:
+					return isCompUnit(list);
+				case V.FuncDef:
+					return isFuncDef(list);
+				case V.FuncType:
+					return isFuncType(list);
+				case V.INT:
+					return isVT(list, V.INT);
+				case V.Ident:
+					return isIdent(list);
+				case V.MAIN:
+					return isVT(list, V.MAIN);
+				case V.SBL:
+					return isVT(list, V.SBL);
+				case V.SBR:
+					return isVT(list, V.SBR);
+				case V.Block:
+					return isBlock(list);
+				case V.LBL:
+					return isVT(list, V.LBL);
+				case V.LBR:
+					return isVT(list, V.LBR);
+				case V.Stmt:
+					return isStmt(list);
+				case V.RETURN:
+					return isVT(list, V.RETURN);
+				case V.Number:
+					return isNumber(list);
+				case V.SEMICOLON:
+					return isVT(list, V.SEMICOLON);
+				case V.DecConst:
+					return isDecConst(list);
+				case V.OctConst:
+					return isOctConst(list);
+				case V.HexConst:
+					return isHexConst(list);
+				case V.NoneZeroDigit:
+					return isNoneZeroDigit(list);
+				case V.Digit:
+					return isDigit(list);
+				case V.OctDigit:
+					return isOctDigit(list);
+				case V.HexPre:
+					return isHexPre(list);
+				case V.HexDigit:
+					return isHexDigit(list);
+			}
+		}catch (Exception e){
+			System.exit(-1);
 		}
 		System.exit(-1);
 		return "";
@@ -89,14 +93,24 @@ public class Recur {
 	public static String catString(String[] grammar, List<String> list) {
 		String ret = "";
 		for (int i = 0; i < grammar.length; i++) {
-			ret += executeV(grammar[i], list, list.size());
+			try{
+				ret += executeV(grammar[i], list, list.size());
+			}catch (Exception e){
+				System.exit(-1);
+			}
 		}
 		return ret;
 	}
 
 	public static String isCompUnit(List<String> list) {
 		String[] grammar = {V.FuncDef};
-		return catString(grammar, list);
+		String s = catString(grammar, list);
+		if(index==list.size()){
+			return s;
+		}else{
+			System.exit(-1);
+			return "err";
+		}
 	}
 
 	public static String isFuncDef(List<String> list) {
